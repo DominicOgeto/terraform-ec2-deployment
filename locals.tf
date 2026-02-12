01837ca0d4abccc326a5c2aeaee382b79fe2a595
DominicOgeto <dominicogeto254@gmail.com>
2026-02-12 07:27:50 +0300

    Update locals.tf

--- locals.tf
+++ locals.tf
@@ -1,16 +1,15 @@
 locals {
-  ingress_rule= [{
-    port = 80
-    cidr_blocks= ["0.0.0.0/0"]
-    protocol = "tcp"
+  ingress_rule = [{
+    port        = 80
+    cidr_blocks = ["0.0.0.0/0"]
+    protocol    = "tcp"
     description = "allow http traffic"
-  },
-  {
-    port = 22
-    cidr_blocks = ["102.219.210.250/32"]
-    protocol = "tcp"
-    description = "allow ssh traffic"
-
-  }
+    },
+    {
+      port        = 22
+      cidr_blocks = ["102.219.210.250/32"]
+      protocol    = "tcp"
+      description = "allow ssh traffic"
+    }
   ]
 }
