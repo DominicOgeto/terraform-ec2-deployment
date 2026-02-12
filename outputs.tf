26cb6bc4091eb7617927632b5e12ee446f6a4e86
DominicOgeto <dominicogeto254@gmail.com>
2026-02-12 07:30:32 +0300

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,4 @@
 output "instance_public_ip" {
-    description = "The Public Ip is:"
-    value = aws_instance.test_server.public_ip
-  
+  description = "The Public Ip is:"
+  value       = aws_instance.test_server.public_ip
 }
